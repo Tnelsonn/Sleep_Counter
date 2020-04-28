ea0baddb1c4849a0357b59d946ff6052ba00ec2b
Tnelsonn <topher4403@gmail.com>
2020-04-28 13:16:20 -0500

    Menu Created

---
+++ sleep_counter.py
@@ -0,0 +1,11 @@
+print("Good Morning!")
+
+choice =''
+
+while choice.upper() != 'Q':
+	print("What would you like to do?")
+	print("S - Create a new log")
+	print("Q - Quit")
+	choice = input("What do you want to do?")
+
+print("Have a good day!")
