75e85be9c739eca939c2e3b53e987cbe2f8a0a79
Tnelsonn <topher4403@gmail.com>
2020-05-04 14:29:41 -0500

    sleep2

--- sleep_counter.py
+++ sleep_counter.py
@@ -7,5 +7,12 @@ while choice.upper() != 'Q':
 	print("S - Create a new log")
 	print("Q - Quit")
 	choice = input("What do you want to do?")
+	
+	if choice == "S":
+		x = input("How Many Hours of sleep did you get?")
+		print(x)
+		print('Good!')
+
+def slpavg(sleep[],days):
 
 print("Have a good day!")
